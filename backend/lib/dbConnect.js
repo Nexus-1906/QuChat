85be0aafc45ae0ddc89078d57bb0788eb6d6053f
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-07 13:34:21 +0530

    Modularize initiation of MongoDB and Redis connection

---
+++ backend/lib/dbConnect.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import redis from 'redis';
+
+export const mongoConnect = () => {
+    try {
+        mongoose.connect(process.env.MONGODB_CONN);
+    }
+    catch (err) {
+        console.error("Unexpected error occurred: Database could not be connected.");
+        console.error(err.message);
+    }
+};
+
+export const redisConnect = async () => {
+    try {
+        const client = redis.createClient({
+            username: 'thestarslayer',
+            password: process.env.REDIS_PASSWORD,
+            socket: {
+                host: 'redis-10401.crce283.ap-south-1-2.ec2.cloud.redislabs.com',
+                port: 10401
+            }
+        });
+
+        await client.connect();
+        return client;
+    }
+    catch (err) {
+        console.error("Unexpected error occurred: Cache could not be connected.");
+        console.error(err.message);
+    } 
+};

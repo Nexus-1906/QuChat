f6d18d32026cdbe8d3209f2c71dca50b6371cd3e
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-07 16:18:40 +0530

    Prevent app to open multiple session of same user

---
+++ backend/middleware/socket.middleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import { redisClient } from "../index.js";
+
+export const ioAuth = async (socket, next) => {
+    const token = socket.handshake.auth.token;
+
+    try {
+        const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const userId = payload.userId;
+        const userExists = !!(await redisClient.hGet(userId));
+
+        if (userExists)
+            throw new Error("User already exists!");
+        
+        next();
+    }
+    catch (err) {
+        next(err);
+    }
+}

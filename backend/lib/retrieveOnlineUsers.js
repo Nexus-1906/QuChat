b990751d0b1d22716b6dece1a6da4426d1e9c4c4
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-12 13:29:35 +0530

    Return null if both cache and DB query fails

--- backend/lib/retrieveOnlineUsers.js
+++ backend/lib/retrieveOnlineUsers.js
@@ -9,7 +9,14 @@ const retrieveOnlineUsers = async (username) => {
     }
     catch (err) {
         console.error("Unexpected error occurred", err.message);
-        onlineUsers = await OnlineUsers.find({ username: { $ne: username } }).project({ _id: 0 });
+
+        try {
+            onlineUsers = await OnlineUsers.find({ username: { $ne: username } }).project({ _id: 0 });
+        }
+        catch (err) {
+            console.error("Unexpected error occurred", err.message);
+            return null;
+        }
     }
 
     return onlineUsers;

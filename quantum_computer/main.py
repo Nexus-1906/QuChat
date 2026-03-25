8ef844abe95822046c580dac48ad1130fa59b0fd
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-25 13:15:25 +0530

    Implement support for multiple shots for QRNG

--- quantum_computer/main.py
+++ quantum_computer/main.py
@@ -18,16 +18,22 @@ q_backend = q_service.least_busy(min_num_qubits=32)
 pm = generate_preset_pass_manager(backend=q_backend, optimization_level=1)
 sampler = Sampler(q_backend)
 
-@app.get("/rng")
-async def random_num_generator():
-    qc = QuantumCircuit(32, 32)
-    qc.h(range(32))
+@app.get("/rng/{bit_length}/{no_of_shots}")
+async def random_num_generator(bit_length, no_of_shots):
+    if (bit_length < 1 or bit_length > 32 or no_of_shots < 1):
+        return
+    
+    qc = QuantumCircuit(bit_length, bit_length)
+    qc.h(range(bit_length))
     qc.measure_all()
     isa_circuit = pm.run(qc)
 
-    job = sampler.run([isa_circuit], shots = 1)
+    job = sampler.run([isa_circuit], shots = no_of_shots)
     result = job.result()
     counts = result[0].data.meas.get_counts()
-    bitstring = max(counts, key=counts.get)
+    
+    bitstrings = []
+    for bitstring in list(counts.keys()):
+        bitstrings.append(bitstring)
 
-    return bitstring
+    return bitstrings

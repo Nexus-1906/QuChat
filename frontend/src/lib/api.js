3b0f0b4b55224573f0514c9a1635454529b61ee9
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-31 21:28:09 +0530

    Implement response interceptor for apiCaller

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import authCaller from "./auth";
 
 const server = import.meta.env.VITE_SERVER_ADDR;
 
@@ -7,5 +8,23 @@ const apiCaller = axios.create({
     headers: { "Authorization": localStorage.getItem("access-token") },
     withCredentials: true
 });
+
+apiCaller.interceptors.response.use(
+    null,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest.retry) {
+            originalRequest.retry = true;
+
+            const authResponse = await authCaller.post("/refresh");
+            localStorage.setItem("access-token", `Bearer ${authResponse.data.accessToken}`);
+            originalRequest.headers.Authorization = localStorage.getItem("access-token");
+            
+            return axios(originalRequest);
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default apiCaller;

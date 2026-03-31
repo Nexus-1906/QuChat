8d2c88f54f9bc0e51c42a1bcae61cd7bcbbe663c
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-31 22:12:32 +0530

    Implement HomePage component with user online status and requests fetching

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,67 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import checkAuth from "../lib/checkAuth";
+import apiCaller from "../lib/api";
+import { toast } from "react-toastify";
+
 function HomePage() {
-    return <>Hello World</>;
+    const [onlineUsers, setOnlineUsers] = useState([]);
+    const [eavesdroppableRequests, setEavesdroppableRequests] = useState([]);
+    const [requestsToMe, setRequestsToMe] = useState([]);
+
+    const [showRequestsToMe, setShowRequestsToMe] = useState(false);
+    const [showEavesdroppableRequests, setShowEavesdroppableRequests] = useState(false);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        checkAuth(navigate, false);
+    }, [navigate]);
+
+    useEffect(() => {
+        async function getOnlineUsers() {
+            try {
+                const response = await apiCaller.get("/getOnlineUsers");
+                setOnlineUsers(response.data.onlineUsers);
+            }
+            catch (error) {
+                console.log("Unexpected error occurred", error);
+                toast.error("Could not get online users!");
+            }
+        };
+
+        getOnlineUsers();
+    }, []);
+
+    useEffect(() => {
+        async function getRequestsToMe() {
+            try {
+                const response = await apiCaller.get("/getMyActiveRequests");
+                setRequestsToMe(response.data.requests);
+            }
+            catch (error) {
+                console.log("Unexpected error occurred", error);
+                toast.error("Could not get your active requests!");
+            }
+        };
+
+        getRequestsToMe();
+    }, []);
+
+    useEffect(() => {
+        async function getEavesdroppableRequests() {
+            try {
+                const response = await apiCaller.get("/getEavesdroppableRequests");
+                setEavesdroppableRequests(response.data.requests);
+            }
+            catch (error) {
+                console.log("Unexpected error occurred", error);
+                toast.error("Could not get eavesdroppable requests!");
+            }
+        };
+
+        getEavesdroppableRequests();
+    }, []);
 }
 
 export default HomePage;

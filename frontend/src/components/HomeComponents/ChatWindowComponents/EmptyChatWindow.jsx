9cf58675aa9a30807734abfba99b568796f01f33
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-04-03 09:13:26 +0530

    Add stubs

--- frontend/src/components/HomeComponents/ChatWindowComponents/EmptyChatWindow.jsx
+++ frontend/src/components/HomeComponents/ChatWindowComponents/EmptyChatWindow.jsx
@@ -1,4 +1,27 @@
-function EmptyChatWindow() {
+function EmptyChatWindow({
+    setShowRequestsToMe, setShowEavesdroppableRequests, setShowNewRequest
+}) {
+    
+    function cbRequestsToMe() {
+        setShowEavesdroppableRequests(false);
+        setShowNewRequest("");
+        setShowRequestsToMe(true);
+    }
+
+    function cbEavesdroppableRequests() {
+        setShowRequestsToMe(false);
+        setShowNewRequest("");
+        setShowEavesdroppableRequests(true);
+    }
+
+    /**
+     * Contains links to RequestsToMe and EavesdroppableRequests
+     * When clicked on a link, use the setState functions provided above to set it to true (all others must be false)
+     */
+    return (
+        <>
+        </>
+    );
 
 }
 
--- frontend/src/components/HomeComponents/ChatWindowComponents/RequestsToMe.jsx
+++ frontend/src/components/HomeComponents/ChatWindowComponents/RequestsToMe.jsx
@@ -1,4 +1,5 @@
 function RequestsToMe() {
+    
 }
 
 export default RequestsToMe;
